5cffe29ab5b6803ba24aa38fcc2d687ae15cfae9
TheWug <rjs3275@g.rit.edu>
2013-03-26 02:00:00 -0400

    got rid of old version

--- trunk/src/Modules.h
+++
@@ -1,9 +0,0 @@
-#ifndef _LIST_OF_MODULES_H_
-#define _LIST_OF_MODULES_H_
-
-#include "Modules/AutoFactoidModule.h"
-#include "Modules/FloodProtectModule.h"
-#include "Modules/Stat.h"
-#include "Modules/CloneWatch.h"
-
-#endif
--- trunk/src/types.h
+++
@@ -1,6 +0,0 @@
-#ifndef _TYPES_H_
-#define _TYPES_H_
-
-enum invite_mode {JOIN, NOTICE, REQUEST, IGNORE};
-
-#endif
